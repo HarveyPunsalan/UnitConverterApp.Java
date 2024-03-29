e618da315bd0de9eb8097f5f389b22a38f1d89c2
Jayson Punsalan <markpunsalan@icloud.com>
2024-03-29 14:43:28 +0800

    Final Commit

---
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.unitconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button btn;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        btn = findViewById(R.id.btn);
+        textView = findViewById(R.id.textView);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Getting the user input (Kilos)
+                String inputText = editText.getText().toString();
+
+                // Converting string into double
+                double kilos = Double.parseDouble(inputText);
+
+                // Converting kilos into pounds
+                double pounds = makeConversion(kilos);
+
+                // Displaying the conversion result
+                textView.setText("" + pounds);
+
+            }
+        });
+    }
+
+    public double makeConversion(double kilos) {
+       // 1 kilogram is equal to 2.20462 pounds
+
+        return kilos * 2.20462;
+    }
+}
